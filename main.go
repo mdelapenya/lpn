58777a009361e0d45ecd0b43dc7492f771848977
Manuel de la Peña <mdelapenya@gmail.com>
2018-02-14 12:46:57 +0100

    If no value is passed, then use current datetime in yyyyMMdd format

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import "fmt"
-import docker "liferay-gowl/docker"
+import (
+	"bufio"
+	"fmt"
+	docker "liferay-gowl/docker"
+	"os"
+	"time"
+
+	"github.com/vjeantet/jodaTime"
+)
 
 func main() {
-	fmt.Print("Enter the Image Tag you want to use for [" + docker.DockerImage + "]: ")
+	var currentDate = jodaTime.Format("YYYYMMdd", time.Now())
+
+	fmt.Println("Enter the Image Tag you want to use for [" + docker.DockerImage + "]")
+	fmt.Print("If you leave it empty, we will use [" + currentDate + "]: ")
+
 	var imageTag string
 
-	fmt.Scanf("%s", &imageTag)
+	reader := bufio.NewReader(os.Stdin)
+	imageTag, _ = reader.ReadString('\n')
+	fmt.Println(imageTag)
+
+	if imageTag != "" {
+		imageTag = currentDate
+	}
 
 	docker.RunDockerImage(getDockerImage(imageTag))
 }
