b8e0ca06d1056962cfdd0b0016a7494caee5be3f
Manuel de la Peña <mdelapenya@gmail.com>
2018-02-15 10:56:43 +0100

    Fix indent

--- cmd/root.go
+++ cmd/root.go
@@ -10,8 +10,8 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "lpn",
 	Short: "lpn (Liferay Portal Nightly) makes it easier to run Liferay Portal's nightly builds.",
-	Long: `A Fast and Flexible CLI for managing Liferay Portal's nightly builds built with
-				love by mdelapenya and friends in Go.`,
+	Long: `A Fast and Flexible CLI for managing Liferay Portal's nightly builds
+				built with love by mdelapenya and friends in Go.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 	},
