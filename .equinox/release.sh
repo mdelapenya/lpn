f583abb9b707da6ce6c7ce6ebdaf8eb47637ad74
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-02-15 21:02:59 +0100

    Use a config.yaml file for the equinox release

--- .equinox/release.sh
+++ .equinox/release.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+readonly DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))"
+
+cat <<EOF > $DIR/config.yaml
+app: app_dK5yVpq7ybd
+signing-key: .equinox/equinox.key
+token: $(cat .equinox/token)
+platforms: [
+  darwin_amd64,
+  linux_amd64,
+  windows_amd64
+]
+EOF
+
 equinox release \
+  --config=".equinox/config.yaml" \
   --version="0.1.1" \
-  --platforms="darwin_amd64 linux_amd64 windows_amd64" \
-  --signing-key=.equinox/equinox.key \
-  --app="app_dK5yVpq7ybd" \
-  --token="$(cat .equinox/token)" \
+  --channel="stable" \
   github.com/mdelapenya/lpn
