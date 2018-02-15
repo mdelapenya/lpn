243f09db3627c55c11c506617ef91da7313e1a3b
Manuel de la Peña <mdelapenya@gmail.com>
2018-02-15 12:24:40 +0100

    Document commands

--- cmd/checkContainer.go
+++ cmd/checkContainer.go
@@ -14,7 +14,7 @@ func init() {
 var checkCmd = &cobra.Command{
 	Use:   "checkContainer",
 	Short: "Check if there is a container created by lpn (Liferay Portal Nightly)",
-	Long:  `Uses docker container inspect to check if there is a container created by lpn (Liferay Portal Nightly)`,
+	Long:  `Uses docker container inspect to check if there is a container with name [` + docker.DockerContainerName + `] created by lpn (Liferay Portal Nightly)`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if docker.CheckDockerContainerExists() {
 			log.Println("The container [" + docker.DockerContainerName + "] is running.")
