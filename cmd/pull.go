c793986c0655ad01c2fcb202ab47c06e2acdf9eb
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-02-15 11:54:59 +0100

    Add pull command

---
+++ cmd/pull.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"errors"
+	date "lpn/date"
+	docker "lpn/docker"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(pullCmd)
+}
+
+var pullCmd = &cobra.Command{
+	Use:   "pull",
+	Short: "Pulls a Liferay Portal nightly Docker image",
+	Long: `Pulls a Liferay Portal nightly Docker image from ` + docker.DockerImage + ` repository.
+	If no image tag is passed to the command, the tag representing the current date [` + date.CurrentDate + `]
+	will be used.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 1 {
+			return errors.New("pull requires zero or one argument representing the image tag to be pulled")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		var tag string
+
+		if len(args) == 0 {
+			tag = date.CurrentDate
+		} else {
+			tag = args[0]
+		}
+
+		docker.PullDockerImage(docker.DockerImage + ":" + tag)
+	},
+}
