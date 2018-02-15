5d2270b6569b7f227070001c46ecb19410c796e7
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-02-15 20:44:17 +0100

    Include windows support on Equinox

--- .equinox/release.sh
+++ .equinox/release.sh
@@ -2,7 +2,7 @@
 
 equinox release \
   --version="0.1.1" \
-  --platforms="darwin_amd64 linux_amd64" \
+  --platforms="darwin_amd64 linux_amd64 windows_amd64" \
   --signing-key=.equinox/equinox.key \
   --app="app_dK5yVpq7ybd" \
   --token="$(cat .equinox/token)" \
