24e433b8fd6bde1d41487822b17fae030b92fb0c
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-02-19 10:42:16 +0100

    (#2) Bump version

--- cmd/version.go
+++ cmd/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of lpn",
 	Long:  `All software has versions. This is lpn (Liferay Portal Nook)`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("lpn (Liferay Portal Nook) v0.1.2 -- HEAD")
+		fmt.Println("lpn (Liferay Portal Nook) v0.2.0 -- HEAD")
 	},
 }
