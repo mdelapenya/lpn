c06b2fd6ddda5eaa75b73ee0554a04b04fa9fe8f
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-03-02 00:02:10 +0100

    Fetch dependant images on Travis' install stage

---
+++ scripts/pull-images.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+readonly NIGHTLY_IMAGE="mdelapenya/liferay-portal-nightlies:latest"
+readonly RELEASE_IMAGE="mdelapenya/liferay-portal:7-ce-ga5-tomcat-hsql"
+
+main() {
+  cat <<EOF >.tmp_images
+${NIGHTLY_IMAGE}
+${RELEASE_IMAGE}
+EOF
+
+  pull_images_concurrently "$(cat .tmp_images)"
+
+  rm .tmp_images
+}
+
+_pull_image() {
+  local image=$1
+  docker pull $image
+}
+
+pull_images_concurrently() {
+  local desired_images="$1"
+  local name
+  local version
+  local image_name
+
+  declare -a pidlist
+  declare -a images
+  declare -a fails
+
+  echo "INFO: Starting to pull images."
+
+  while read -r image_name; do
+    printf "\n\e[1;31m Pushing: [$image_name] \e[0m\n"
+    _pull_image $image_name &
+    pidlist+=($!)
+    images+=($image_name)
+  done<<<"$desired_images"
+
+  for index in "${!pidlist[@]}"; do
+    local image=${images[$index]}
+    local pid=${pidlist[$index]}
+
+    echo "waiting on pid $pid"
+    if ! wait $pid; then
+      fails+=($image)
+    fi
+  done
+
+  set +o nounset
+  if ! [[ ${#fails[@]} -eq 0 ]]; then
+    printf "\n\e[1;31m Some image pulls failed: \e[0m\n"
+    for fail in ${fails[@]}; do
+      printf "\e[1;31m - $fail \e[0m\n"
+    done
+
+    exit 1
+  fi
+}
+
+stop_pulls() {
+  printf "\e[1;31mStopping pulls\e[0m\n"
+
+  kill -- -$(ps -o pgid=$$ | grep -o '[0-9]*')
+}
+
+trap stop_pulls INT
+
+main
