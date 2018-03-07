61b7dc4c45157db8d9342132f81d4be9d5ef5be3
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-03-07 09:12:15 +0100

    Bump version

--- assets/license/license.go
+++ assets/license/license.go
@@ -83,7 +83,7 @@ func licenseTxt() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "LICENSE.txt", size: 1512, mode: os.FileMode(420), modTime: time.Unix(1520240954, 0)}
+	info := bindataFileInfo{name: "LICENSE.txt", size: 1512, mode: os.FileMode(420), modTime: time.Unix(1520776257, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
