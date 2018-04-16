f02f219bcab4a4a2c0e94030cab253561e9a30ce
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-04-16 09:11:18 +0200

    [#17] Always check if Docker is installed before using lpn

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/mdelapenya/lpn/cmd"
+
+	docker "github.com/mdelapenya/lpn/docker"
 )
 
 func main() {
+	installed := docker.CheckDocker()
+
+	if !installed {
+		log.Fatalln(`Docker is not installed. Please visit "https://docs.docker.com/install/#desktop" to install it before using lpn`)
+	}
+
 	cmd.Execute()
 }
