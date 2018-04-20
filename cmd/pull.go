30d3edf11391a65fcfafabb11f8ecc335a8d3299
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-04-21 00:59:46 +0200

    (#22) SImplify pull command

--- cmd/pull.go
+++ cmd/pull.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 
+	date "github.com/mdelapenya/lpn/date"
 	docker "github.com/mdelapenya/lpn/docker"
 	liferay "github.com/mdelapenya/lpn/liferay"
 
@@ -15,6 +16,18 @@ var forceRemoval bool
 
 func init() {
 	rootCmd.AddCommand(pullCmd)
+
+	pullCmd.AddCommand(pullCommerce)
+	pullCommerce.Flags().BoolVarP(&forceRemoval, "forceRemoval", "f", false, "Removes the cached, local image, if exists")
+	pullCommerce.Flags().StringVarP(&tagToPull, "tag", "t", date.CurrentDate, "Sets the image tag to pull")
+
+	pullCmd.AddCommand(pullNightly)
+	pullNightly.Flags().BoolVarP(&forceRemoval, "forceRemoval", "f", false, "Removes the cached, local image, if exists")
+	pullNightly.Flags().StringVarP(&tagToPull, "tag", "t", date.CurrentDate, "Sets the image tag to pull")
+
+	pullCmd.AddCommand(pullRelease)
+	pullRelease.Flags().BoolVarP(&forceRemoval, "forceRemoval", "f", false, "Removes the cached, local image, if exists")
+	pullRelease.Flags().StringVarP(&tagToPull, "tag", "t", "latest", "Sets the image tag to pull")
 }
 
 var pullCmd = &cobra.Command{
@@ -34,6 +47,63 @@ var pullCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		SubCommandInfo()
+	},
+}
+
+var pullCommerce = &cobra.Command{
+	Use:   "commerce",
+	Short: "Pulls a Liferay Portal Docker image from Commerce Builds",
+	Long: `Pulls a Liferay Portal Docker image from the Commerce Builds repository: "` + liferay.CommercesRepository + `".
+	If no image tag is passed to the command, the tag representing the current date [` + date.CurrentDate + `] will be used.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 1 {
+			return errors.New("pull commerce requires zero or one argument representing the image tag to be pulled")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		commerce := liferay.Commerce{Tag: tagToPull}
+
+		PullDockerImage(commerce, forceRemoval)
+	},
+}
+
+var pullNightly = &cobra.Command{
+	Use:   "nightly",
+	Short: "Pulls a Liferay Portal Docker image from Nightly Builds",
+	Long: `Pulls a Liferay Portal Docker image from the Nighlty Builds repository: "` + liferay.NightliesRepository + `".
+	If no image tag is passed to the command, the tag representing the current date [` + date.CurrentDate + `] will be used.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 1 {
+			return errors.New("pull nightly requires zero or one argument representing the image tag to be pulled")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		nightly := liferay.Nightly{Tag: tagToPull}
+
+		PullDockerImage(nightly, forceRemoval)
+	},
+}
+
+var pullRelease = &cobra.Command{
+	Use:   "release",
+	Short: "Pulls a Liferay Portal Docker image from releases",
+	Long: `Pulls a Liferay Portal instance, obtained from the unofficial releases repository: "` + liferay.ReleasesRepository + `".
+	If no image tag is passed to the command, the "latest" tag will be used.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 1 {
+			return errors.New("pull nightly requires zero or one argument representing the image tag to be pulled")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		release := liferay.Release{Tag: tagToPull}
+
+		PullDockerImage(release, forceRemoval)
 	},
 }
 
