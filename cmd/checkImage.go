e233e3df163856e1a2c33db4c5ed817aeb187d30
Manuel de la Peña <mdelapenya@gmail.com>
2018-04-21 00:51:32 +0200

    (#22) Simplify chekImage command

--- cmd/checkImage.go
+++ cmd/checkImage.go
@@ -14,6 +14,15 @@ var tagToCheck string
 
 func init() {
 	rootCmd.AddCommand(checkImageCmd)
+
+	checkImageCmd.AddCommand(checkImageCommerce)
+	checkImageCommerce.Flags().StringVarP(&tagToCheck, "tag", "t", "latest", "Sets the image tag to check")
+
+	checkImageCmd.AddCommand(checkImageNightly)
+	checkImageNightly.Flags().StringVarP(&tagToCheck, "tag", "t", "latest", "Sets the image tag to check")
+
+	checkImageCmd.AddCommand(checkImageRelease)
+	checkImageRelease.Flags().StringVarP(&tagToCheck, "tag", "t", "latest", "Sets the image tag to check")
 }
 
 var checkImageCmd = &cobra.Command{
@@ -32,6 +41,69 @@ var checkImageCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		SubCommandInfo()
+	},
+}
+
+var checkImageCommerce = &cobra.Command{
+	Use:   "commerce",
+	Short: "Checks if the proper Liferay Portal with Commerce Build image has been pulled by lpn",
+	Long: `Checks if the proper Liferay Portal with Commerce Build image has been pulled by lpn.
+	Uses docker image inspect to check if the proper Liferay Portal with Commerce image has 
+	been pulled by lpn (Liferay Portal Nook). If no image tag is passed to the command,
+	the tag "latest" will be used.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 1 {
+			return errors.New("checkImage commerce requires zero or one argument representing the image tag")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		commerce := liferay.Commerce{Tag: tagToCheck}
+
+		CheckImage(commerce)
+	},
+}
+
+var checkImageNightly = &cobra.Command{
+	Use:   "nightly",
+	Short: "Checks if the proper Liferay Portal Nightly Build image has been pulled by lpn",
+	Long: `Checks if the proper Liferay Portal Nightly Build image has been pulled by lpn.
+	Uses docker image inspect to check if the proper Liferay Portal image has 
+	been pulled by lpn (Liferay Portal Nook). If no image tag is passed to the command,
+	the tag "latest" will be used.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 1 {
+			return errors.New("checkImage nightly requires zero or one argument representing the image tag")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		nightly := liferay.Nightly{Tag: tagToCheck}
+
+		CheckImage(nightly)
+	},
+}
+
+var checkImageRelease = &cobra.Command{
+	Use:   "release",
+	Short: "Checks if the proper Liferay Portal release image has been pulled by lpn",
+	Long: `Checks if the proper Liferay Portal release image has been pulled by lpn.
+	Uses docker image inspect to check if the proper Liferay Portal image has 
+	been pulled by lpn (Liferay Portal Nook). If no image tag is passed to the command,
+	the tag "latest" will be used.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 1 {
+			return errors.New("checkImage release requires zero or one argument representing the image tag")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		release := liferay.Release{Tag: tagToCheck}
+
+		CheckImage(release)
 	},
 }
 
