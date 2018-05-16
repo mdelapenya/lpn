398ed981ed28fc9e0b99a25b1677950c6a8395cc
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-05-16 17:24:59 +0200

    Bump v0.6.0 version

--- assets/version/version.go
+++ assets/version/version.go
@@ -68,7 +68,7 @@ func (fi bindataFileInfo) Sys() interface{} {
 	return nil
 }
 
-var _versionTxt = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\x32\xd0\x33\xd5\x33\x00\x04\x00\x00\xff\xff\x6d\x89\x90\x28\x05\x00\x00\x00")
+var _versionTxt = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\x32\xd0\x33\xd3\x33\x00\x04\x00\x00\xff\xff\x34\x37\xd6\x2a\x05\x00\x00\x00")
 
 func versionTxtBytes() ([]byte, error) {
 	return bindataRead(
@@ -83,7 +83,7 @@ func versionTxt() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "VERSION.txt", size: 5, mode: os.FileMode(420), modTime: time.Unix(1524270379, 0)}
+	info := bindataFileInfo{name: "VERSION.txt", size: 5, mode: os.FileMode(420), modTime: time.Unix(1526484257, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
