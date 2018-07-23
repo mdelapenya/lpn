d54aa5e88c12fca1e038a763dc633ae9af53b119
Manuel de la Peña <mdelapenya@gmail.com>
2018-07-23 13:28:27 +0200

    Fix typo

--- cmd/checkContainer.go
+++ cmd/checkContainer.go
@@ -35,7 +35,7 @@ var checkContainerCommerceCmd = &cobra.Command{
 	Use:   "commerce",
 	Short: "Checks if there is a Commerce container created by lpn",
 	Long: `Checks if there is a Commerce container created by lpn (Liferay Portal Nook).
-	Uses docker container inspect to check if there is a Commerce container with name [lpn-commere] created by lpn (Liferay Portal Nook)`,
+	Uses docker container inspect to check if there is a Commerce container with name [lpn-commerce] created by lpn (Liferay Portal Nook)`,
 	Run: func(cmd *cobra.Command, args []string) {
 		commerce := liferay.Commerce{}
 
