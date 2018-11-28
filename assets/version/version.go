405c73e1291cff49655d546f2eaa6f40aba37fae
Manuel de la Peña <mdelapenya@gmail.com>
2018-11-27 23:06:51 -0800

    Update static files

--- assets/version/version.go
+++ assets/version/version.go
@@ -83,7 +83,7 @@ func versionTxt() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "VERSION.txt", size: 5, mode: os.FileMode(420), modTime: time.Unix(1543156769, 0)}
+	info := bindataFileInfo{name: "VERSION.txt", size: 5, mode: os.FileMode(420), modTime: time.Unix(1543388617, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
