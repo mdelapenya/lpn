dda781d082f7946568375760a56004153ffed000
Manuel de la Peña <mdelapenya@gmail.com>
2019-02-13 18:05:02 +0100

    (#62) Change snapshot features in more places

--- cmd/pull.go
+++ cmd/pull.go
@@ -126,7 +126,7 @@ var pullNightly = &cobra.Command{
 	Use:   "nightly",
 	Short: "Pulls a Liferay Portal Docker image from Nightly Builds",
 	Long: `Pulls a Liferay Portal Docker image from the Nightly Builds repository: "` + liferay.NightliesRepository + `".
-	If no image tag is passed to the command, the tag representing the current date [` + date.CurrentDate + `] will be used.`,
+	If no image tag is passed to the command, the "master" tag will be used.`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) > 1 {
 			return errors.New("pull nightly requires zero or one argument representing the image tag to be pulled")
