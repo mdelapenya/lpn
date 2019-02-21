74bf24ebd99de921ad5a70c2cd5b8f33d096649d
Manuel de la Peña <mdelapenya@gmail.com>
2019-02-21 12:25:35 +0100

    (#70) Bump

--- assets/version/version.go
+++ assets/version/version.go
@@ -83,7 +83,7 @@ func versionTxt() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "VERSION.txt", size: 6, mode: os.FileMode(420), modTime: time.Unix(1550743914, 0)}
+	info := bindataFileInfo{name: "VERSION.txt", size: 6, mode: os.FileMode(420), modTime: time.Unix(1550748288, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
